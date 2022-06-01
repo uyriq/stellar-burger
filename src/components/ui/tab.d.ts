453d5c4224bd718b038fd7e4b68441ff761fb555
uyriq <shi32bckp@yandex.kz>
2022-06-01 22:10:51 +0300

    adopt ui from react-developer-burger-ui-components, try to fix children for 18 REACT, remaster components

---
+++ src/components/ui/tab.d.ts
@@ -0,0 +1,2 @@
+interface Props{children:React.ReactNode;}
+
---
+++ src/components/ui/tab.tsx
@@ -0,0 +1,21 @@
+import React, {PropsWithChildren} from 'react';
+import tab from './tab.module.css';
+
+export const Tab: React.FC<PropsWithChildren <{
+    active: boolean;
+    value: string;
+    onClick: (value: string) => void;
+}>> = ({ active, value, children, onClick: handleClick }) => {
+    const className = `${tab.tab} ${active ? tab.tab_type_current : ''}`;
+    const onClick = React.useCallback(() => {
+        if (typeof handleClick === 'function') {
+            handleClick(value);
+        }
+    }, [handleClick, value]);
+
+    return (
+        <div className={`${className} pt-4 pr-10 pb-4 pl-10 noselect`} onClick={onClick}>
+            <span className="text text_type_main-default">{children}</span>
+        </div>
+    );
+};
