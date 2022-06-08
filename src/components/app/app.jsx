c5dd9178f39035e3bd48e7b34c0127e38574aa34
uyriq <uyriq@local>
2022-06-08 15:14:37 +0300

    chore(format): format code

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -24,18 +24,15 @@ const App = (props) => {
 
     useEffect(() => {
      
-        
         let res
         const getIngredients = async () => {
             try {
-                
                 res = await fetch(`${apiBaseUrl}${apiEndpoints.ingredients}`)
                 if (!res.ok) throw new Error('fetch trouble') } catch (e) {
                     console.info(`облом - ${e.message}`);
                     setIngredients (ingredients => ( {...ingredients, error: true}))
                 }
                 let _ = await res.json()
-                  
                      setIngredients (ingredients => ( {...ingredients, success:_.success, data:_.data}))
 
 
@@ -47,38 +44,29 @@ getIngredients();
 console.log(!!ingredients)
 const {success, error, data} = ingredients;
 return !!data && (
-    <> 
+    <>
 <div className={[Styles.supper_container].join(' ').concat(' ')}>
-    
             <header className={[Styles.app].join(' ').concat(' mr-30 pr-30 ')}>
             <AppHeader />
             {error && <p className={ [Styles.spinner].join(' ').concat(' text_color_error p-2 ')}> Что-то пошло не так, не получены данные </p>}
-    
             {(!success && !error) && <div className={Styles.spinner}>
                 <ClipLoader color={'#ffff'} loading={!success} size={550} />
             </div>}
             </header>
 
             { !!success && !!!error && <div className={[Styles.supper_container_inner].join(' ').concat(' ')}>
-                
                 <main className={[Styles.app].join(' ').concat(' ')}>
                 <BurgerIngredients ingredients={data} /> 
                 </main>
-                
                 <main>
                 <BurgerConstructor ingredients={data} />
                 </main>
-                    
             </div> 
-            
             }
             { !!data && <div className='text p-2 '>неподвижный footer</div> }
 </div>
 
-
     </>
-
-
 
 )
 }
