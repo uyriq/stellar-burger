df5055c9908d5d10831095d2e19b4f568401f995
uyriq <uyriq@local>
2022-09-04 21:36:07 +0300

    eslint, prettier all code reformat

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,18 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './components/app/app';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/app/app'
 import './index.css'
 
-const rootElement = document.getElementById("root") as HTMLElement;
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root') as HTMLElement
+const root = createRoot(rootElement)
 
 root.render(
     <StrictMode>
         <App />
     </StrictMode>
-);
-
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals(console.log);
-
